6ecf8f08eb1985b43c800add06c433cc7f10e18a
andreykusman <andreyestudo18@gmail.com>
2024-10-24 15:30:35 -0300

    feat: adicionar páginas de login e senha

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import MainDashboard from "./pages/main-dashboard/MainDashboard";
 import { loader } from "./pages/main-dashboard/InfoTable";
+import React from 'react';
+import Login from './pages/Login/Login';
+import Password from "./pages/Password/Password";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainDashboard />,
     loader: loader,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Password",
+    element: <Password />,
   },
 ]);
 
