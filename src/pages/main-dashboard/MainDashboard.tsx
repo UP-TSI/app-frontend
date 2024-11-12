c9b8b7eb05e5d56edde841024078d357abc7b760
Gabriel <gabrielpepe.pp@gmail.com>
2024-11-12 19:52:07 -0300

    feat: add token funcionalities by cookie to login page

--- src/pages/main-dashboard/MainDashboard.tsx
+++ src/pages/main-dashboard/MainDashboard.tsx
@@ -1,13 +1,23 @@
+import Cookies from "js-cookie";
 import Footer from "./Footer";
 import Header from "./Header";
 import InfoTable from "./InfoTable";
 import Statistic from "./Statistic";
+import { useNavigate } from "react-router-dom";
 
 const MainDashboard = () => {
+  const userCookieInfo = Cookies.get("userCookieInfo");
+  const navigate = useNavigate();
+  const userCookieInfoObj = JSON.parse(userCookieInfo || "{}");
+
+  if (!userCookieInfo) {
+    navigate("/user-login");
+  }
+
   return (
     <>
       <div className="flex bg-gradient-to-b from-[#00AAFF42] to-white to-25% flex-col p-16">
-        <Header />
+        <Header username={userCookieInfoObj.username} />
         <InfoTable />
         <Statistic />
       </div>
